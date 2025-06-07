d79e7aaa04e35657159d7fdd719836952924c778
osenbal <iqbalmaulana172703@gmail.com>
2025-06-07 19:55:46 +0700

    add additional link

---
+++ src/app/(public)/help-app/page.tsx
@@ -0,0 +1,11 @@
+export default function HelpApp() {
+  return (
+    <section className="mx-auto max-w-[1440px] px-4  mt-8">
+      <h3 className="text-center text-2xl font-semibold">
+        Bantuan
+        <br />
+        Applikasi Pantau.ai
+      </h3>
+    </section>
+  );
+}
