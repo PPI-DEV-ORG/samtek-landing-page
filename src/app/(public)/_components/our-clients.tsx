af229de4636d1bab4d488ce302471ff7a7766ce5
osenbal <iqbalmaulana172703@gmail.com>
2025-07-21 16:00:40 +0700

    change carousel our client section

--- src/app/(public)/_components/our-clients.tsx
+++ src/app/(public)/_components/our-clients.tsx
@@ -1,13 +1,26 @@
-import Image from 'next/image';
+'use client';
+
 import React from 'react';
+import Image from 'next/image';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
 
 const logos = ['logo-1.png', 'logo-2.png', 'logo-3.png', 'logo-4.png', 'logo-5.png', 'logo-6.png', 'logo-7.png', 'logo-8.png'];
 
 export const OurClientsSection: React.FC = () => {
+  const plugin = useRef(
+    Autoplay({
+      delay: 2000,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+    })
+  );
+
   return (
     <section className="py-10 bg-gray-100 dark:bg-gray-900 sm:py-16 lg:py-24">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="grid items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 sm:gap-x-12 gap-y-12">
+        <div className="grid grid-cols-1 lg:grid-cols-5 gap-y-12 gap-x-3">
           <div className="lg:col-span-2">
             <h2 className="text-3xl font-bold leading-tight text-gray-800 dark:text-white sm:text-4xl lg:text-5xl lg:leading-tight">
               1 team.
@@ -23,23 +36,25 @@ export const OurClientsSection: React.FC = () => {
             </p>
           </div>
 
-          <div className="lg:col-span-3 xl:col-span-4">
-            <div className="grid items-center max-w-4xl grid-cols-2 mx-auto lg:grid-cols-4 gap-x-10 gap-y-16">
-              {logos.map((logo, index) => {
-                const isHiddenOnSmall = index >= 4;
-                return (
-                  <div key={logo} className={isHiddenOnSmall ? 'hidden lg:block' : ''}>
-                    <Image width={50} height={50} className="object-contain w-full h-8 mx-auto" src={`/images/our-clients/${index + 1}.png`} alt={`Logo ${index + 1}`} />
-                  </div>
-                );
-              })}
-            </div>
-
-            <div className="flex items-center justify-start mt-10 space-x-3 lg:hidden">
-              <div className="w-2.5 h-2.5 rounded-full bg-blue-600"></div>
-              <div className="w-2.5 h-2.5 rounded-full bg-gray-300"></div>
-              <div className="w-2.5 h-2.5 rounded-full bg-gray-300"></div>
-            </div>
+          <div className="lg:col-span-3 xl:col-span-3 flex items-center">
+            <Carousel
+              opts={{
+                align: 'start',
+                loop: true,
+              }}
+              plugins={[plugin.current]}
+              className="w-full"
+            >
+              <CarouselContent className="-ml-4">
+                {logos.map((logo, index) => (
+                  <CarouselItem key={index} className="pl-4 basis-1/2 md:basis-1/3 lg:basis-1/4">
+                    <div className="flex items-center justify-center p-4 bg-white dark:bg-gray-800 rounded-md shadow-md h-28">
+                      <Image width={100} height={100} src={`/images/our-clients/${index + 1}.png`} alt={`Logo ${index + 1}`} className="object-contain h-16 w-auto" />
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
           </div>
         </div>
       </div>
